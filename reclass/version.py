27f2fa730455341fe218f60e9141d54e904b1023
Andrew Pickford <anpickford@googlemail.com>
2017-09-05 10:32:55 +0200

    bump version to 1.5.1

--- reclass/version.py
+++ reclass/version.py
@@ -8,11 +8,11 @@
 #
 RECLASS_NAME = 'reclass'
 DESCRIPTION = 'merge data by recursive descent down an ancestry hierarchy (forked extended version)'
-VERSION = '1.5'
+VERSION = '1.5.1'
 AUTHOR = 'martin f. krafft / Andrew Pickford'
 AUTHOR_EMAIL = 'andrewp@nikhef.nl'
 MAINTAINER = 'Andrew Pickford'
 MAINTAINER_EMAIL = ''
-COPYRIGHT = 'Copyright © 2007–14 martin f. krafft, extentions © 2017 Andrew Pickford'
+COPYRIGHT = 'Copyright © 2007–14 martin f. krafft, extensions © 2017 Andrew Pickford'
 LICENCE = 'Artistic Licence 2.0'
 URL = 'https://github.com/AndrewPickford/reclass'
