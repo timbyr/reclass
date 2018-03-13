ac4ab035810b7087615d1bcd27ce0a70e246b08c
Andrew Pickford <anpickford@googlemail.com>
2018-03-13 14:55:03 +0100

    fix bug introduced when fixing merging of escaped strings, when a parameters object is not initialised by merging the objects internal copy of the initial dictionary must be a deep copy.

--- reclass/datatypes/parameters.py
+++ reclass/datatypes/parameters.py
@@ -57,7 +57,7 @@ class Parameters(object):
                 self.merge(mapping)
                 self._keep_overrides = False
             else:
-                self._base = mapping
+                self._base = copy.deepcopy(mapping)
 
     #delimiter = property(lambda self: self._delimiter)
 
