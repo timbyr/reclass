b803adb533aee3c50aa2fa902e4442d4891e39c0
Andrew Pickford <anpickford@googlemail.com>
2020-06-12 10:53:32 +0200

    Allow for class mappings to wildcard match against either the node name or node file path
    By default a class mapping will match against the name of a node:
    
    class_mappings:
      - test*        alpha
    
    This will match all nodes names starting with test.
    
    A possibly unintended side effect of the original implementation allowed
    for matches against the path of the node file. So for a node file
    'test/node1.yml' a match:
    
    class_mappings:
      - test/*        alpha
    
    would work. A reworking of the class mappings implementation ment this
    matching stopped working. The type of matching done by the class mapping
    is now controlled by the configuration option class_mappings_match_path.
    If false (the default) the match is done against the node name and if true
    the match is done again the path.

--- reclass/settings.py
+++ reclass/settings.py
@@ -19,6 +19,7 @@ class Settings(object):
         'allow_dict_over_scalar': defaults.OPT_ALLOW_DICT_OVER_SCALAR,
         'allow_none_override': defaults.OPT_ALLOW_NONE_OVERRIDE,
         'automatic_parameters': defaults.AUTOMATIC_RECLASS_PARAMETERS,
+        'class_mappings_match_path': defaults.CLASS_MAPPINGS_MATCH_PATH,
         'default_environment': defaults.DEFAULT_ENVIRONMENT,
         'delimiter': defaults.PARAMETER_INTERPOLATION_DELIMITER,
         'dict_key_override_prefix':
